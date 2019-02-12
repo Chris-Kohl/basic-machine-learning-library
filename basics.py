f9c875c4578bcdfcdb3495bf5af2013bc408c60e
SilentPrevail <christiankohl97@yahoo.com>
2019-02-12 11:21:34 +0000

    First basic functions
    Some functions, also taught at the coursera machine learning course, changed for python. https://www.coursera.org/learn/machine-learning

---
+++ basics.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def idenfunc(x):
+    zeroes = np.zeros((x, x))
+    for y in range(0, x):
+        zeroes[y, y] = 1
+    return zeroes
+
+
+def plotgraph(x, y):
+    plt.plot(x)
+    plt.ylabel(y)
+    plt.show()
+
+
+def addleftcolumns(x):
+    ones = np.ones((x.shape[0], 1))
+    y = np.concatenate((ones, x), axis=1)
+    return y
+
+
+def inittheta(x):
+    theta = np.ones((1, x.shape[0]))
+    return theta
+
+
+def basichypothesis(x, t):
+    theta = t.T
+    hypothesis = theta * x
+    return hypothesis
