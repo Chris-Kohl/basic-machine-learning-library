90f5103c19b2e26a51c0e1e9c56abc7a73cbdefc
SilentPrevail <christiankohl97@yahoo.com>
2019-02-15 11:55:20 +0000

    Matrix manipulation class made, functions moved

--- basics.py
+++ basics.py
@@ -2,23 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def idenfunc(x):
-    zeroes = np.zeros((x, x))
-    for y in range(0, x):
-        zeroes[y, y] = 1
-    return zeroes
-
-
 def plotgraph(x, y):
     plt.plot(x)
     plt.ylabel(y)
     plt.show()
-
-
-def addleftcolumns(x):
-    ones = np.ones((x.shape[0], 1))
-    y = np.concatenate((ones, x), axis=1)
-    return y
 
 
 def inittheta(x):
@@ -30,10 +17,3 @@ def basichypothesis(x, t):
     theta = t.T
     hypothesis = theta * x
     return hypothesis
-
-
-def heavisidefunction(x):
-    if x >= 0:
-        return 1
-    else:
-        return -1
---
+++ matrixOperations.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def idenfunc(x):
+    zeroes = np.zeros((x, x))
+    for y in range(0, x):
+        zeroes[y, y] = 1
+    return zeroes
+
+
+def addNewFeature(x, y):
+    ones = np.ones((x.shape[0], 1))
+    if y == 0:
+        z = np.concatenate((ones, x), axis=1)
+    else:
+        z = np.concatenate((x, ones), axis=1)
+    return z
